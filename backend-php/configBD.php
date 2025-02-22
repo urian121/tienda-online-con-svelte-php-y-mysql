0910db0ad2992f24f1423003626aba50bcaab7f9
URIAN VIERA <urian1213viera@gmail.com>
2025-02-22 17:48:17 -0500

    Update configBD.php

--- backend-php/configBD.php
+++ backend-php/configBD.php
@@ -1,4 +1,9 @@
 <?php
+// Habilitar errores para depuración
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'tb_tienda_svelte_php');
 define('DB_USER', 'root');
